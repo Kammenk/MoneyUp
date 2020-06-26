43e5b9d5dbe8730cc216308892dcf7e0e9ff57a6
Kammenk <35629844+Kammenk@users.noreply.github.com>
2020-06-26 19:39:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/moneyup/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.moneyup
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.schedule
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        moveForward()
+    }
+
+    private fun moveForward(){
+
+        val delay: Long = 1500
+        val intent = Intent(this,LoginActivity::class.java)
+        Timer().schedule(delay) {
+            startActivity(intent)
+        }
+    }
+}
